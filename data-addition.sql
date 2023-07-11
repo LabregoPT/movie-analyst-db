ddeb363e499968c8c131c244a9622697c99e2bc4
Jhon Edward Mora <jhonedmora@gmail.com>
2023-07-11 14:11:55 -0500

    Added checks for a pending review, and added pending reviews from API

--- data-addition.sql
+++ data-addition.sql
@@ -14,10 +14,13 @@ INSERT INTO movie_db.reviewer (name, avatar, publication) VALUES ('Mindy Lee','h
 INSERT INTO movie_db.reviewer (name, avatar, publication) VALUES ('Martin Thomas','https://s3.amazonaws.com/uifaces/faces/twitter/karsh/128.jpg','TheOne');
 INSERT INTO movie_db.reviewer (name, avatar, publication) VALUES ('Anthony Miller','https://s3.amazonaws.com/uifaces/faces/twitter/9lessons/128.jpg','ComicBookHero.com');
 
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Deadpool', '2016', 11, 'Robert Smith');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Thor: Ragnarok', '2017', 7, 'Chris Harris');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('It', '2017', 8, 'Janet Garcia');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Dunkirk', '2017', 8, 'Andrew West');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Logan', '2017', 8, 'Mindy Lee');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Batman V Superman', '2016', 6, 'Martin Thomas');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Mad Max: Fury Road', '2015', 6, 'Anthony Miller');
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication) VALUES ('Deadpool', '2016', 9, 'Robert Smith', 'The Daily Reviewer');
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication) VALUES ('Thor: Ragnarok', '2017', 7, 'Chris Harris', 'International Movie Critic');
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication) VALUES ('It', '2017', 8, 'Janet Garcia', 'MoviesNow');
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication) VALUES ('Dunkirk', '2017', 8, 'Andrew West', 'MyNextReview');
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication) VALUES ('Logan', '2017', 8, 'Mindy Lee', 'Movies and Games');
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication) VALUES ('Batman V Superman', '2016', 6, 'Martin Thomas', 'TheOne');
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication) VALUES ('Mad Max: Fury Road', '2015', 6, 'Anthony Miller', 'ComicBookHero.com');
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Superman: Homecoming', '2017', 10, 'Chris Harris', 'International Movie Critic', true);
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Wonder Woman', '2017', 8, 'Martin Thomas', 'TheOne', true;
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Doctor Strange', '2016', 7, 'Anthony Miller', 'ComicBookHero.com', true);
--- table-creation.sql
+++ table-creation.sql
@@ -1,4 +1,12 @@
 CREATE DATABASE movie_db CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE TABLE movie_db.publication (name VARCHAR(255) PRIMARY KEY, avatar VARCHAR(21));
 CREATE TABLE movie_db.reviewer (name VARCHAR(255) PRIMARY KEY, avatar VARCHAR(255), publication VARCHAR(255), FOREIGN KEY (publication) REFERENCES publication(name) ON DELETE CASCADE);
-CREATE TABLE movie_db.moviereview (title VARCHAR(255) PRIMARY KEY, `release` VARCHAR(255), score INTEGER, reviewer VARCHAR(255), FOREIGN KEY (reviewer) REFERENCES reviewer(name) ON DELETE CASCADE);
+CREATE TABLE movie_db.moviereview (title VARCHAR(255) PRIMARY KEY, 
+								   `release` VARCHAR(255),
+									score INTEGER,
+                                    reviewer VARCHAR(255),
+                                    publication VARCHAR(255),
+                                    pending boolean default false,
+                                    FOREIGN KEY (reviewer) REFERENCES reviewer(name),
+                                    FOREIGN KEY (publication) REFERENCES reviewer(publication)
+                                    ON DELETE CASCADE);
